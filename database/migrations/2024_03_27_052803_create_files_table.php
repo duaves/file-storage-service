9fd3e8a7a72028efdf99ff126fbcc4dac9677fae
duaves <denis.titov.dua@gmail.com>
2024-03-27 11:37:52 +0500

    first commit

---
+++ database/migrations/2024_03_27_052803_create_files_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('files', function (Blueprint $table) {
+            //id файла
+            $table->id();
+
+            // Название файла введенное пользователем
+            $table->string('name')->nullable();
+
+            // Оригинальное название файла
+            $table->string('original_name');
+
+            // Расширение файла
+            $table->string('extension');
+
+            // Размер файла
+            $table->double('size');
+
+            // Временные метки для создания и изменения записей
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('files');
+    }
+};
